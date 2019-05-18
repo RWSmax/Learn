f45fc44fd00f9a6e5041a3b4a0231c4b851be428
RWSmax <damien+line@bk.ru>
2019-05-18 14:59:25 +0300

    1.1.2 error

---
+++ str_template_missing.py
@@ -0,0 +1,10 @@
+import string
+
+values = {'var': 'test'}
+
+t = string.Template("$var is here but $missing is not provided.")
+try:
+    print('substitute():', t.substitute(values))
+except KeyError as err:
+    print('Error:', str(err))
+print('safe_substitute():',t.safe_substitute(values))
